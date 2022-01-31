c98d8307e1a64916fdd514ccfca11abf49b1e274
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-01-31 17:50:50 +0100

    refactor

--- src/main/java/com/java/academy/GameOfLife.java
+++ src/main/java/com/java/academy/GameOfLife.java
@@ -1,18 +1,31 @@
 package com.java.academy;
 
-import static java.util.Objects.deepEquals;
+import java.util.Arrays;
 
 public class GameOfLife {
     public int[] nextGeneration(int[] cell) {
+        int[] result = Arrays.copyOf(cell, cell.length);
 
-        if (deepEquals(cell, new int[] {1,1,1})) {
-            return new int[]{0, 1, 0};
+        for(int i = 0; i<cell.length; i++){
+            if(i == 0){
+                result[i] = 0;
+                continue;
+            }
+
+            if(i == (cell.length -1)){
+                result[i] = 0;
+                continue;
+            }
+
+            if(cell[i-1] == 0 || cell[i+1] == 0 ){
+                result[i] = 0;
+            };
+
+            if(cell[i-1] == 1 && cell[i+1] == 1){
+                result[i] = cell[i] == 1 ? 1 : 0;
+            }
         }
 
-        if (deepEquals(cell, new int[] {1,1,1,1})) {
-            return new int[]{0, 1, 1,0};
-        }
-
-        return new int[]{0, 0, 0};
+        return result;
     }
 }
