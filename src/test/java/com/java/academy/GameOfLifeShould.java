d2463971d38f6b70dc1a4ce05c2b6141487145fd
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-02-01 11:52:52 +0100

    refactor

--- src/test/java/com/java/academy/GameOfLifeShould.java
+++ src/test/java/com/java/academy/GameOfLifeShould.java
@@ -9,55 +9,65 @@ public class GameOfLifeShould {
     @Test void
     return_one_dead_ceLl_when_entering_one_dead_cell(){
         var game = new GameOfLife();
+        int[][] expected = {{0}};
+
         var result = game.start(new int[][]{{0}});
 
-        assertArrayEquals(new int[][]{{0}}, result);
+        assertArrayEquals(expected, result);
     }
 
     @Test void
     return_two_dead_cells_when_entering_two_dead_cells(){
         var game = new GameOfLife();
+        int[][] expected = {{0, 0}};
+
         var result = game.start(new int[][]{{0,0}});
 
-        assertArrayEquals(new int[][]{{0, 0}}, result);
+        assertArrayEquals(expected, result);
     }
 
     @Test void
     return_three_dead_cells_when_entering_three_dead_cells(){
         var game = new GameOfLife();
+        int[][] expected = {{0, 0, 0}};
+
         var result = game.start(new int[][]{{0,0,0}});
 
-        assertArrayEquals(new int[][]{{0, 0, 0}}, result);
+        assertArrayEquals(expected, result);
     }
 
     @Test void
     return_three_dead_cells_when_entering_two_dead_cells_and_one_alive(){
         var game = new GameOfLife();
+        int[][] expected = {{0, 0, 0}};
+
         var result = game.start(new int[][]{{1,0,0}});
 
-        assertArrayEquals(new int[][]{{0, 0, 0}}, result);
+        assertArrayEquals(expected, result);
     }
 
     @Test void
     return_four_dead_cells_when_entering_two_dead_cells_and_two_alive(){
         var game = new GameOfLife();
+        int[][] expected = {{0, 0, 0, 0}};
+
         var result = game.start(new int[][]{{0,1,1,0}});
 
-        assertArrayEquals(new int[][]{{0, 0, 0, 0}}, result);
+        assertArrayEquals(expected, result);
     }
 
     @Test void
     return_all_dead_cells_when_entering_non_evolving_positioned_live_cells(){
         var game = new GameOfLife();
-        var result = game.start(new int[][]{
-            {1,0,1,0},
-            {0,0,0,1}
-        });
-
         var expected = new int[][] {
             {0,0,0,0},
             {0,0,0,0}
         };
+
+        var result = game.start(new int[][]{
+            {1,0,1,0},
+            {0,0,0,1}
+        });
 
         assertArrayEquals(expected, result);
     }
