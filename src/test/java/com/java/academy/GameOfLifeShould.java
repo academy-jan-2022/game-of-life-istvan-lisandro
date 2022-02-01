78893cf8d21601ea6aa26fbcc4692a9bb5ae86f1
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-02-01 10:50:03 +0100

    red

--- src/test/java/com/java/academy/GameOfLifeShould.java
+++ src/test/java/com/java/academy/GameOfLifeShould.java
@@ -30,4 +30,11 @@ public class GameOfLifeShould {
         assertArrayEquals(new int[][]{{0, 0, 0}}, result);
     }
 
+    @Test void
+    should_return_three_dead_cells_when_entering_two_dead_cells_and_one_alive(){
+        var game = new GameOfLife();
+        var result = game.start(new int[][]{{1,0,0}});
+
+        assertArrayEquals(new int[][]{{0, 0, 0}}, result);
+    }
 }
