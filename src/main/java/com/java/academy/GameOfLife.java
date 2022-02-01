c2a432cf371cd24cebdb7ee374840655fe8b70a2
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-02-01 11:25:55 +0100

    green

--- src/main/java/com/java/academy/GameOfLife.java
+++ src/main/java/com/java/academy/GameOfLife.java
@@ -6,9 +6,12 @@ public class GameOfLife {
     public int[][] start(int[][] world) {
         var result = Arrays.copyOf(world, world.length);
 
-        for (int i = 0; i < world[0].length; i++) {
-            if (isItAlive(world[0], i)) {
-                killCell(result[0], i);
+
+        for (int i = 0; i < world.length; i++) {
+            for(int j = 0; j < world[i].length; j++){
+                if (isItAlive(world[i], j)) {
+                    killCell(result[i], j);
+                }
             }
         }
 
