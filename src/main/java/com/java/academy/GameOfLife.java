c790e75678e645afb3b8843b96ba336f76442f2d
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-02-01 15:24:14 +0100

    refactor

--- src/main/java/com/java/academy/GameOfLife.java
+++ src/main/java/com/java/academy/GameOfLife.java
@@ -12,25 +12,29 @@ public class GameOfLife {
 
         for (int i = 0; i < world.getRows(); i++) {
             for(int j = 0; j < world.getColumns(); j++){
-                if(j == 0 || j == world.getColumns() -1){
-                    continue;
-                }
-
-                if (world.isCellAlive(i, j) &&
-                    world.isCellAlive(i, j-1) &&
-                    world.isCellAlive(i, j+1)
-                ) {
-                    killCell(result[i], j-1);
-                    bringCellToLife(result[i], j);
-                    killCell(result[i], j+1);
-
-                } else {
-                    killCell(result[i], j);
-                }
+                updateWorld(result, i, j);
             }
         }
 
         return new World(result);
+    }
+
+    private void updateWorld(int[][] result, int i, int j) {
+        if(j == 0 || j == world.getColumns() -1){
+            return;
+        }
+
+        if (world.isCellAlive(i, j) &&
+            world.isCellAlive(i, j -1) &&
+            world.isCellAlive(i, j +1)
+        ) {
+            killCell(result[i], j -1);
+            bringCellToLife(result[i], j);
+            killCell(result[i], j +1);
+
+        } else {
+            killCell(result[i], j);
+        }
     }
 
     private void killCell(int[] row, int position) {
