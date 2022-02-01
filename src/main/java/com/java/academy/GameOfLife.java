6e9a9654f25479935506d6ee6ec8140f2433ee60
iagardi <garbigz@gmail.com>
2022-02-01 09:39:56 +0000

    red test

--- src/main/java/com/java/academy/GameOfLife.java
+++ src/main/java/com/java/academy/GameOfLife.java
@@ -1,35 +1,7 @@
 package com.java.academy;
 
-import java.util.Arrays;
-
 public class GameOfLife {
-    public int[] nextGeneration(int[] cell) {
-        int[] result = Arrays.copyOf(cell, cell.length);
-
-        for(int i = 0; i<cell.length; i++){
-            if(i == 0){
-                result[i] = 0;
-                continue;
-            }
-
-            if(i == (cell.length -1)){
-                result[i] = 0;
-                continue;
-            }
-
-            if(!isCellAlive(cell[i-1]) || !isCellAlive(cell[i+1])){
-                result[i] = 0;
-            };
-
-            if(isCellAlive(cell[i-1]) && isCellAlive(cell[i+1])) {
-                result[i] = cell[i] == 1 ? 1 : 0;
-            }
-        }
-
-        return result;
-    }
-
-    private boolean isCellAlive(int cell) {
-        return cell == 1;
+    public int[][] start(int[][] world) {
+        return new int[0][];
     }
 }
--- src/test/java/com/java/academy/GameOfLifeShould.java
+++ src/test/java/com/java/academy/GameOfLifeShould.java
@@ -1,4 +1,17 @@
 package com.java.academy;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertArrayEquals;
+
 public class GameOfLifeShould {
+
+    @Test void
+    should_return_one_dead_ceLl_when_entering_one_dead_cell(){
+        var game = new GameOfLife();
+        var result = game.start(new int[][]{{0}});
+
+        assertArrayEquals(new int[][]{{0}}, result);
+    }
+
 }
