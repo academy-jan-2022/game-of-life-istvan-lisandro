72de42066caedee06b099568889ec771d5a587a0
Lisandro Bertoli <lisandro.bertoli@gmail.com>
2022-02-01 10:49:57 +0100

    refactor

--- src/main/java/com/java/academy/GameOfLife.java
+++ src/main/java/com/java/academy/GameOfLife.java
@@ -2,14 +2,7 @@ package com.java.academy;
 
 public class GameOfLife {
     public int[][] start(int[][] world) {
-        if(world[0].length == 3){
-            return new int[][]{{0, 0, 0}};
-        }
 
-        if (world[0].length == 2) {
-            return new int[][] {{0,0}};
-        }
-
-        return new int[][]{{0}};
+        return world;
     }
 }
